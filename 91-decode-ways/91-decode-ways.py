94547ccc415ebe0bc3f3c639b38cee9b1b090d76
kalika15 <86293635+kalika15@users.noreply.github.com>
2022-10-01 18:58:07 +0530

    Time: 39 ms (84.58%), Space: 13.7 MB (98.57%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -19,23 +19,4 @@ class Solution:
                     r=self.solve(string,i+2,dp)
             dp[i]=l+r
             return dp[i]
-        '''def solve(s,i,DP):
-            if i>=len(s):
-                return 1
-            else:
-                if s[i]=='0':
-                    return 0
-                if DP[i]!=-1:
-                    return DP[i]
-                
-                a=b=0
-                a=solve(s,i+1,DP)
-                if i<len(s)-1:
-                    num=int(s[i:i+2])
-                    if num<=26:
-                        b=solve(s,i+2,DP)
-                DP[i]= a+b
-                return DP[i]
-        DP=[-1 for i in range(len(s)+1)]
-        return solve(s,0,DP)'''
-    
+        
