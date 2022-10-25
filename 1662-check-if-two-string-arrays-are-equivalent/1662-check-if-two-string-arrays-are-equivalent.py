34f71508eb9191d7b82e07c64e9ac87828472b54
kalika15 <86293635+kalika15@users.noreply.github.com>
2022-10-25 11:23:07 +0530

    Time: 70 ms (16.21%), Space: 13.9 MB (75.82%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        a=""
+        b=""
+        for i in word1:
+            a+=i
+        for i in word2:
+            b+=i
+        if a==b:
+            return True
+        return False
