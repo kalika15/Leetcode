e2e150ec11691b64a6997b07505ca3f94e5b3bc6
kalika15 <86293635+kalika15@users.noreply.github.com>
2023-04-07 14:05:24 +0530

    Time: 1223 ms (5.01%), Space: 14.2 MB (72.89%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        self.solve(nums,0,result)
+        return result
+    def swap(self,a,b,nums):
+        nums[a],nums[b]=nums[b],nums[a]
+    def solve(self,nums,start,result):
+        if start==len(nums)-1 and (nums[::] not in result):
+            result.append(nums[::])
+        for i in range(start,len(nums)):
+            self.swap(i,start,nums)
+            self.solve(nums,start+1,result)
+            self.swap(i,start,nums)
