a452273c71f13f0131836f221335b78253a285b0
kalika15 <86293635+kalika15@users.noreply.github.com>
2023-04-25 18:41:07 +0530

    Time: 2 ms (54.95%), Space: 43.2 MB (74.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,30 +1,55 @@
-class Solution {
-    int m,n;
-    public int findCircleNum(int[][] isConnected) {
-        m=isConnected.length;
-        n=isConnected[0].length;
-        int[] visited=new int[m];
-        int c=0;
-        for(int i=0;i<m;i++){
-            visited[i]=0;
+class UnionFind{
+    private int[] parent;
+    private int[] rank;
+    private int count=1;
+    public UnionFind(int size){
+        this.parent=new int[size];
+        this.rank=new int[size];
+        this.count=size;
+        for(int i=0; i<size; i++){
+            this.parent[i]=i;
+            this.rank[i]=1;
         }
-        for(int i=0;i<m;i++){
-            if(visited[i]==0){
-                dfs(i,visited,isConnected);
-                c++;
-            }
-        }
-        return c;
     }
-    public void dfs(int k,int[] visited,int[][] isConnected){
-        if(visited[k]!=0){
-            return;
+    public int find(int u){
+        if(u==this.parent[u]){
+            return u;
         }
-        visited[k]=1;
-        for(int i=0;i<n;i++){
-            if(isConnected[k][i]==1){
-                    dfs(i,visited,isConnected);
+        return parent[u]=find(parent[u]);
+    }
+    public void union(int u,int v){
+        int p1=find(u);
+        int p2=find(v);
+        if(p1!=p2){
+            if(rank[p1]>rank[p2]){
+                this.parent[p2]=p1;
+            }
+            else if(rank[p1]<rank[p2]){
+                this.parent[p1]=p2;
+            }
+            else{
+                parent[p2]=p1;
+                rank[p1]+=1;
+            }
+            this.count-=1;
+        }
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        UnionFind obj=new UnionFind(isConnected.length);
+        for(int i=0; i<isConnected.length; i++){
+            for(int j=0; j<isConnected.length; j++){
+                if(isConnected[i][j]==1){
+                    obj.union(i,j);
+                }
             }
         }
+
+        return obj.getCount();
     }
 }
